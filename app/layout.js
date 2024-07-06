437c2adacf0d94727f03b3ac996c309b6c2fef98
nishant <mahtonishantraj@gmail.com>
2024-07-06 17:22:22 +0530

    updated recaptcha

--- app/layout.js
+++ app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
         </main>
         <Footer />
       </body>
-      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM } />
     </html>
   );
 }
