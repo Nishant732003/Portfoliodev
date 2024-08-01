cbacae500223f3bae74a091d1a0f72077f08ac53
Nishant Raj <98076817+Nishant732003@users.noreply.github.com>
2024-08-01 22:20:21 +0530

    Update index.jsx

--- app/components/homePage/hero-section/index.jsx
+++ app/components/homePage/hero-section/index.jsx
@@ -112,7 +112,7 @@ function HeroSection() {
               <div>
                 <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
                 <span className="text-gray-400">{`'`}</span>
-                <span className="text-amber-300">Abu Said</span>
+                <span className="text-amber-300">Nishant Raj</span>
                 <span className="text-gray-400">{`',`}</span>
               </div>
               <div className="ml-4 lg:ml-8 mr-2">
@@ -126,7 +126,7 @@ function HeroSection() {
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">Express</span>
                 <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">NestJS</span>
+{/*                 <span className="text-amber-300">NestJS</span> */}
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">MySql</span>
                 <span className="text-gray-400">{"', '"}</span>
@@ -134,7 +134,7 @@ function HeroSection() {
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">Docker</span>
                 <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">AWS</span>
+{/*                 <span className="text-amber-300">AWS</span> */}
                 <span className="text-gray-400">{"'],"}</span>
               </div>
               <div>
